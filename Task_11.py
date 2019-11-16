306c14366c8d0c01d6c34e4c3bbdfcfc9de4eb1f
NelinVitaliy <s5001481.nelin@gmail.com>
2019-11-16 15:12:25 +0600

    Chapter_2__Task_11

---
+++ Task_11.py
@@ -0,0 +1,10 @@
+# # string = "Makers is very cool and informative bootcamp!"
+# string = input('Print something here: ')
+# string = string.strip()
+# search = (" ")
+# spaces = string.count(search)
+# word_count = str(spaces + 1)
+# print(('Word count in your string: ') + word_count)
+
+
+#
